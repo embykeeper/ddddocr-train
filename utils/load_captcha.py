cdf82b008ab445b67d918651789c3e26c5e195e8
jackzzs <jackzzs@outlook.com>
2023-05-08 16:54:02 +0800

    Train misty

---
+++ utils/load_captcha.py
@@ -0,0 +1,16 @@
+import uuid
+from pathlib import Path
+
+from typer import Typer, Argument
+
+app = Typer()
+
+@app.command()
+def main(inp: Path = Argument(..., exists=True, file_okay=False)):
+    for f in inp.iterdir():
+        hash = str(uuid.uuid4()).replace('-', '')
+        print(f.with_stem(f'{f.stem}_{hash}'))
+        f.rename(f.with_stem(f'{f.stem}_{hash}'))
+
+if __name__ == '__main__':
+    app()
---
+++ utils/test_captcha.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from ddddocr import DdddOcr
+from typer import Typer, Argument
+from PIL import Image
+
+app = Typer()
+
+@app.command()
+def digits(proj: Path = Argument(..., exists=True, file_okay=False), inp: Path = Argument(..., exists=True)):
+    if inp.is_file():
+        inps = [inp]
+    else:
+        inps = list(inp.iterdir())
+    for inp in inps:
+        model = next((proj/ 'models').glob('*.onnx'))
+        charsets = proj / 'models' / 'charsets.json'
+        ocr = DdddOcr(show_ad=False, import_onnx_path=str(model), charsets_path=str(charsets))
+        captcha = ocr.classification(Image.open(inp))
+        print(f'{inp.name}: {captcha}')
+    
+if __name__ == '__main__':
+    app()
